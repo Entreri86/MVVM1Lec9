6fd31858fbfbbeb456127d13b7ae8cc09629ead9
Fernando Otal Barja <otal.fernando@gmail.com>
2018-08-12 16:18:12 +0200

    Agregar archivos de proyecto.

---
+++ MVVM1Lec9/MVVM1Lec9/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MVVM1Lec9
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
